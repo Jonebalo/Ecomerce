93e5d0f5e58b77ac59352971a9248215e1751975
@jonebalo <jonebalo@hotmail.com>
2023-06-06 10:04:02 +0200

    Cambios Ecomerce

---
+++ JS/login.js
@@ -0,0 +1,29 @@
+let loginForm;
+
+function inicializar() {
+    loginForm = new FormularioLogin()
+    loginForm.render();
+}
+
+function FormularioLogin() {
+    this.render = function() {
+        const formularioLoginDiv = document.getElementById("formulario-login");
+        formularioLoginDiv.innerHTML = `<input type="email" id="email" />
+            <input type="password" id="password" />
+            <button onclick="loginForm.login()">Login</button>`;
+    },
+    
+    this.login = function() {
+        console.log("Boton login clickado");
+
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
+
+        if (email === "israel@mail.com" && password === "12345678") {
+            window.location.href =  "index.html"
+            console.log("Usuario logueado");
+        } else {
+            console.log("Usuario no valido");
+        }
+    }
+}
