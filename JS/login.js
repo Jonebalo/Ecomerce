d4d3ec910b8961b58b4f41a68a4b1c9a0dbeb6d4
@jonebalo <jonebalo@hotmail.com>
2023-11-24 16:50:23 +0100

    Changes

--- JS/login.js
+++ JS/login.js
@@ -15,15 +15,14 @@ function FormularioLogin() {
     
     this.login = function() {
         console.log("Boton login clickado");
-
         const email = document.getElementById("email").value;
         const password = document.getElementById("password").value;
 
-        if (email === "israel@mail.com" && password === "12345678") {
+        if (email === "user@mail.com" && password === "1234abcd") {
             window.location.href =  "index.html"
             console.log("Usuario logueado");
         } else {
             console.log("Usuario no valido");
         }
     }
-}
+} 
